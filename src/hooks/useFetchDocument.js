c3c50a373d2ca246267c0ff2c3742d4f34eb611f
Diogo Berner <diogoberner@gmail.com>
2022-06-08 20:53:24 -0300

    Single Post

---
+++ src/hooks/useFetchDocument.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+import { db } from "../firebase/config";
+import { doc, getDoc } from "firebase/firestore";
+
+export const useFetchDocument = (docCollection, id) => {
+  const [documents, setDocuments] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  const [cancelled, setCancelled] = useState(false);
+
+  useEffect(() => {
+    async function loadDocument() {
+      if (cancelled) return;
+
+      setLoading(true);
+
+      try {
+        const docRef = await doc(db, docCollection, id);
+        const docSnap = await getDoc(docRef);
+
+        setDocuments(docSnap.data());
+
+        setLoading(false);
+      } catch (error) {
+        setError(error.message);
+        console.log(error);
+
+        setLoading(false);
+      }
+    }
+
+    loadDocument();
+  }, [cancelled, docCollection, id]);
+
+  useEffect(() => {
+    return () => setCancelled(true);
+  }, []);
+
+  return { documents, loading, error };
+};
